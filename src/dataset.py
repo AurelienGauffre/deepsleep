609210c99c81f5f3ad4b2f0632e7ac8a6f81a178
Aurélien <aurelien.gauffre@gmail.com>
2020-05-04 12:54:38 +0200

    Add transom class ToTensor

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import torch
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, utils
 
@@ -14,24 +15,26 @@ class SoundDataset(Dataset):
     class DatasetFolderNotFound(Exception):
         pass
 
-    def __init__(self, name, transform=None):
+    def __init__(self, dataset_name, transform, data_nature='2D'):
         """
         Args:
-            name (string): Directory with all the images.
+            dataset_name (string): Directory with all the images.
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-
-        self.root_dir = SoundDataset.DATA_FOLDER / name
+        self.dataset_name = dataset_name
+        self.root_dir = SoundDataset.DATA_FOLDER / dataset_name
         if not self.root_dir.exists():
             raise SoundDataset.DatasetFolderNotFound(
-                f'No folder called \'{name}\' found in the dataset '
+                f'No folder called \'{dataset_name}\' found in the dataset '
                 f'folder{SoundDataset.DATA_FOLDER}'
             )
         self.sounds_list = list(self.root_dir.rglob('*.wav'))
         self.classes = [f.stem for f in self.root_dir.glob('*/') if f.is_dir()]
         self.nb_classes = len(self.classes)
-        self.transform = transform
+        self.data_nature = data_nature
+        # TODO: Define a proper init_transforms
+        self.transform = transforms.Compose([ToTensor(self.data_nature)])
 
     def __len__(self):
         return len(self.sounds_list)
@@ -41,13 +44,38 @@ class SoundDataset(Dataset):
         #     idx = idx.tolist()
         sound_path = self.sounds_list[idx]
         label = sound_path.parent.stem  # label is the folder name
-        sound = Sound(path=sound_path, process=False)
+        sound = Sound(
+            path=sound_path,
+            process=False if self.data_nature == '1D' else True,
+        )
         sample = {'sound': sound, 'label': label}
 
         if self.transform:
             sample = self.transform(sample)
-
         return sample
 
 
+class ToTensor(object):
+    """Convert ndarrays to Tensors. This class is used as a transform in the
+    data loading` """
 
+    class ModeNotRecognised(Exception):
+        pass
+
+    def __init__(self, data_nature):
+        self.data_nature = data_nature
+
+    def __call__(self, sample):
+
+        sound, label = sample['sound'], sample['label']
+        if self.data_nature == '1D':
+            return {'sound': torch.from_numpy(sound.y), 'label': label}
+        elif self.data_nature == '2D':
+            return {
+                'sound': torch.from_numpy(sound.spectrogram),
+                'label': label,
+            }
+        else:
+            raise ToTensor.ModeNotRecognised(
+                "Enter a valid value for " "data_nature."
+            )
