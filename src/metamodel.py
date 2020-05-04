9ac9894ff0ff79d7ff69e3666c108f31d2213ab8
Aurélien <aurelien.gauffre@gmail.com>
2020-05-05 01:16:28 +0200

    Add a dedicated  dataloader for test set

--- src/metamodel.py
+++ src/metamodel.py
@@ -6,23 +6,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision import transforms
 
-from dataset import SoundDataset
+from dataset import SoundDataset, ToTensor
 
 
 class Metamodel:
     def __init__(
         self,
-        dataset_name=None,
-        data_nature=None,
-        loss_fc=None,
-        optimizer=None,
-        learning_rate=None,
-        nb_epochs=None,
-        batch_size=None,
-        scheduler=None,
+        dataset_name,
+        data_nature,
+        transform,
+        loss_fc,
+        optimizer,
+        learning_rate,
+        nb_epochs,
+        batch_size,
+        test_size,
         network=None,
+        scheduler=None,
     ):
         self.device = torch.device(
             "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -30,38 +33,53 @@ class Metamodel:
         self.nb_epochs = nb_epochs
         self.batch_size = batch_size
         self.loss_fc = loss_fc
-        self.log_interval = 2
         self.dataset_name = dataset_name
         self.learning_rate = learning_rate
         self.data_nature = data_nature
+        self.test_size = test_size
+        self.log_interval = 10
 
-        self.dataset = self.init_dataset()
-        self.network = self.init_network()
+        self.transform = self.init_transform(transform)
+        (
+            self.dataset,
+            self.dataset_train,
+            self.dataset_test,
+        ) = self.init_dataset()
         self.train_loader, self.test_loader = self.init_data_loader()
+        self.network = self.init_network()
         self.optimizer = self.init_optimizer(optimizer)
         self.scheduler = self.init_scheduler()
 
     def init_network(self) -> nn.Module:
-        return self.network(self.dataset.nb_labels)  # we instantiate the
-        # network here
+        return self.network(self.dataset.nb_labels)  # network is instantiated
 
-    def init_dataset(self) -> torch.utils.data.Dataset:
-        return SoundDataset(
+    def init_transform(self, transform):
+        return transforms.Compose([ToTensor(self.data_nature)] + transform)
+
+    def init_dataset(self) -> Tuple[torch.utils.data.Dataset, ...]:
+
+        dataset = SoundDataset(
             dataset_name=self.dataset_name,
-            transform=None,
+            transform=self.transform,
             data_nature=self.data_nature,
         )
+        len_test = int(len(dataset) * self.test_size)
+        dataset_train, dataset_test = random_split(
+            dataset, [len(dataset) - len_test, len_test]
+        )
+        return dataset, dataset_train, dataset_test
 
     def init_data_loader(self) -> Tuple[torch.utils.data.DataLoader, ...]:
+
         train_loader = DataLoader(
-            self.dataset,
+            self.dataset_train,
             batch_size=self.batch_size,
             shuffle=True,
             num_workers=4,
         )
-        # TODO Design a proper test data loader
+
         test_loader = DataLoader(
-            self.dataset,
+            self.dataset_test,
             batch_size=self.batch_size,
             shuffle=False,
             num_workers=4,
