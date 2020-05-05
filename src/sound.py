65d0f01fe1cfc2f6ae57b62f6269583664fef561
Aurélien <aurelien.gauffre@gmail.com>
2020-05-05 17:41:34 +0200

    Add handling of sounds smaller than stepsize

--- src/sound.py
+++ src/sound.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 import librosa  # Librosa for audio
+import librosa.util
 import librosa.display  # and the display module for visualization
 import matplotlib.pyplot as plt
 import numpy as np
@@ -71,11 +72,16 @@ class Sound:
         Take the sound in self.y and split in into samples of size sample_size.
 
         Args:
-            mode: Set to one of 3 values : 'drop','duplicate','keep' to select
-            how to deal with last sample. Drop will drop the sample,
-            keep will keep a sample from an unknown size, and 'duplicate'
-            will add a small part from the previous sample to make the last
-            one of equal size.
+            mode(str, optional):
+                'duplicate' (default)
+                    Add a  part from the previous sample to make the last
+                    one of equal size
+                'drop'
+                    Drop last sample.
+                'keep'
+                    Keep last sample from an (unknown size)
+
+
             sample_length: length of each sample in seconds.
             output_folder: Folder where samples are saved.
             suffix: String add at the end of the files.
@@ -85,43 +91,55 @@ class Sound:
         step_size = int(sample_length * self.sampling_rate)
         i = 0
         n = len(self.y)
-        for i in range(n // step_size):
+        if n < step_size: # sound is smaller than required sample size
             librosa.output.write_wav(
                 os.path.join(
                     output_folder, f'{self.filebase}_{suffix}_{i}.wav'
                 ),
-                self.y[i * step_size : (i + 1) * step_size],
+                np.pad(self.y, (0, step_size - n)), # 0 padding at the end
                 sr=self.sampling_rate,
                 norm=False,
             )
-        if n % step_size != 0:  # if the last sample is smaller than the others
-            i = n // step_size
-            if mode == 'drop':
-                pass
-            elif mode == 'duplicate':
+        else:
+            for i in range(n // step_size):
                 librosa.output.write_wav(
                     os.path.join(
                         output_folder, f'{self.filebase}_{suffix}_{i}.wav'
                     ),
-                    self.y[n - step_size : n],
+                    self.y[i * step_size : (i + 1) * step_size],
                     sr=self.sampling_rate,
                     norm=False,
                 )
-            elif mode == 'keep':
-                librosa.output.write_wav(
-                    os.path.join(
-                        output_folder, f'{self.filebase}_{suffix}_{i}.wav'
-                    ),
-                    self.y[i * step_size : n],
-                    sr=self.sampling_rate,
-                    norm=False,
-                )
+            if n % step_size != 0:  # if the last sample is smaller than the others
+                i = n // step_size
+                if mode == 'drop':
+                    pass
+                elif mode == 'duplicate':
+                    librosa.output.write_wav(
+                        os.path.join(
+                            output_folder, f'{self.filebase}_{suffix}_{i}.wav'
+                        ),
+                        self.y[n - step_size : n],
+                        sr=self.sampling_rate,
+                        norm=False,
+                    )
+                elif mode == 'keep':
+                    librosa.output.write_wav(
+                        os.path.join(
+                            output_folder, f'{self.filebase}_{suffix}_{i}.wav'
+                        ),
+                        self.y[i * step_size : n],
+                        sr=self.sampling_rate,
+                        norm=False,
+                    )
 
 
 if __name__ == '__main__':
-    for file in os.listdir('../../Talk'):
+    folder = '../../Sleep'
+    for file in os.listdir(folder):
         print(file)
-        s1 = Sound(os.path.join('../../Talk/', file))
-        # s1.to_samples(sample_length=4, mode='duplicate')
-        s1.plot_spectrogram()
-        plt.show()
+        s1 = Sound(os.path.join(folder, file),process=False)
+        s1.to_samples(sample_length=4, mode='duplicate')
+       # s1.plot_spectrogram()
+        #plt.show()
+        #print(s1.y.shape)
