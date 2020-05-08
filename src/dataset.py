60303dbb3fcc0759c0055cec2b09fe8c4bb4cad4
Aurélien <aurelien.gauffre@gmail.com>
2020-05-08 02:55:11 +0200

    Add spectrogram in dataloader and refactor data processing

--- src/dataset.py
+++ src/dataset.py
@@ -16,12 +16,12 @@ class SoundDataset(Dataset):
         pass
 
     def __init__(
-        self,
-        dataset_name,
-        transform,
-        data_nature='2D',
-        sampling_rate=None,
-        process_data='Auto',
+            self,
+            dataset_name,
+            transform,
+            data_nature='2D',
+            sampling_rate=None,
+            process_data='Auto',
     ):
         """
         Args:
@@ -34,17 +34,15 @@ class SoundDataset(Dataset):
         self.sampling_rate = sampling_rate
         self.root_dir = SoundDataset.DATA_FOLDER / dataset_name
         self.raw_dir = self.root_dir / 'raw'
-        self.processed_dir = self.root_dir / f'processed_{data_nature}'
+        self.processed_dir = self.root_dir / f'processed'
         if process_data == 'Auto':
             self.process_data = not (self.processed_dir.exists())
         else:
             self.process_data = process_data
         if self.process_data:
-            self.make_processing(data_nature)
+            self.make_processing()
 
-
-
-        self.sounds_list = list(self.processed_dir.rglob('*.wav'))
+        self.files_list = list(self.processed_dir.rglob('*1D.*'))
         self.labels = [
             f.stem for f in self.processed_dir.glob('*/') if f.is_dir()
         ]
@@ -54,57 +52,69 @@ class SoundDataset(Dataset):
         self.transform = transform
 
     def __len__(self):
-        return len(self.sounds_list)
+        return len(self.files_list)
 
     def __getitem__(self, idx):
         # if torch.is_tensor(idx):
         #     idx = idx.tolist()
-        sound_path = self.sounds_list[idx]
-        label = sound_path.parent.stem  # label is the folder name
+        file = Path(self.files_list[idx])
+        label = file.parent.stem  # label is the folder name
         sound = Sound(
-            path=sound_path,
+            path=file,
             process=False if self.data_nature == '1D' else True,
             sampling_rate=self.sampling_rate,
         )
+        spectrogram = np.load(file.parent / f'{file.stem[:-2]}2D.npy')
 
-        sample = {'sound': sound, 'label': self.label_to_num[label]}
+        sample = {'sound': sound,'spectrogram':spectrogram ,'label':
+            self.label_to_num[
+            label]}
         sample = self.transform(sample)
         return sample
 
-    def make_processing(self,data_nature):
+    def make_processing(self):
         """ Process all the raw data"""
-        if not self.root_dir.exists(): # require dataset folder
+        if not self.root_dir.exists():  # require dataset folder
             raise SoundDataset.DatasetFolderStructureError(
                 f'No folder called \'{self.dataset_name}\' found in the '
                 f'dataset '
                 f'folder{SoundDataset.DATA_FOLDER}'
             )
-        if self.process_data:  # processing all the data
-            if not self.raw_dir.exists():  # require raw data folder
-                raise SoundDataset.DatasetFolderStructureError(
-                    f'To process data, raw data have to be located in \'data'
-                    f'/{self.dataset_name}/raw/\' '
-                )
 
-            self.processed_dir.mkdir(exist_ok=True)
-            for label in [
-                f.stem for f in self.raw_dir.glob('*/') if f.is_dir()
-            ]:
-                Path(self.root_dir / f'processed_1D' / label).mkdir(
-                    exist_ok=True
-                )
-            for file in list(self.raw_dir.rglob('*.wav')):
-                sound = Sound(
-                    file,
-                    process=not (data_nature == '1D'),
-                    sampling_rate=self.sampling_rate,
-                )
+        if not self.raw_dir.exists():  # require raw data folder
+            raise SoundDataset.DatasetFolderStructureError(
+                f'To process data, raw data have to be located in \'data'
+                f'/{self.dataset_name}/raw/\' '
+            )
 
-                sound.to_samples(
-                    output_folder=self.processed_dir / file.parent.stem
-                )
-            if self.data_nature == '2D':
-                pass  # TODO
+        self.processed_dir.mkdir(exist_ok=True)
+
+        labels = [f.stem for f in self.raw_dir.glob('*/') if f.is_dir()]
+        for label in labels:
+            Path(self.processed_dir / label).mkdir(
+                exist_ok=True
+            )
+        # 1D Process
+        for file in list(self.raw_dir.rglob('*.wav')):
+            sound = Sound(
+                file,
+                process=False,
+                sampling_rate=self.sampling_rate,
+            )
+            sound.to_samples(
+                output_folder=self.processed_dir / file.parent.stem,
+                suffix='1D'
+            )
+        # 2D Process
+        for file in list(self.processed_dir.rglob('*1D.wav')):
+            sound = Sound(
+                file,
+                process=True,
+                sampling_rate=None,
+            )
+            np.save(Path(file.parent / f'{file.stem[:-2]}2D.npy'),
+                    sound.spectrogram)
+
 
 class ToTensor(object):
     """Convert ndarrays to Tensors. This class is used as a transform in the
