061ab2572b95eabc3d7b3bff898d14076f1bd88a
Aurélien <aurelien.gauffre@gmail.com>
2020-05-10 11:35:39 +0200

    Add mel-frequency cepstrum coefficient

--- src/sound.py
+++ src/sound.py
@@ -17,19 +17,21 @@ class Sound:
         pass
 
     def __init__(
-            self,
-            path: Path = None,
-            sampling_rate: int = None,
-            process: bool = True,
+        self,
+        path: Path = None,
+        sampling_rate: int = None,
+        process: bool = True,
     ):
         self.path = path
         self.folder, self.filename = path.parent, path.name
         self.filebase, self.ext = os.path.splitext(self.filename)
         self.y, self.sampling_rate = librosa.load(self.path, sr=sampling_rate)
         self.spectrogram = None
+        self.mfcc = None
 
         if process:
             self.spectrogram = self.compute_spectrogram()
+            self.mfcc = self.compute_mfcc()
 
     def compute_spectrogram(self, n_mels: int = 256):
         """
@@ -40,10 +42,20 @@ class Sound:
         """
         return librosa.power_to_db(
             librosa.feature.melspectrogram(
-                self.y, sr=self.sampling_rate, n_mels=n_mels
+                y=self.y, sr=self.sampling_rate, n_mels=n_mels
             ),
             ref=np.max,
         )  # convert to log scale (dB). Use the peak power (max) as ref
+
+    def compute_mfcc(self):
+        return librosa.feature.mfcc(
+            y=self.y,
+            sr=self.sampling_rate,
+            S=None,
+            n_mfcc=20,
+            dct_type=2,
+            norm='ortho',
+        )
 
     def plot_spectrogram(self, ax=None):
         if self.spectrogram is None:
@@ -62,11 +74,11 @@ class Sound:
         ax.set_title('mel power spectrogram')
 
     def to_samples(
-            self,
-            sample_length: float = 4,
-            mode: str = 'duplicate',
-            output_folder: str = None,
-            suffix: str = 'sample',
+        self,
+        sample_length: float = 4,
+        mode: str = 'duplicate',
+        output_folder: str = None,
+        suffix: str = 'sample',
     ):
         """
         Take the sound in self.y and split in into samples of size sample_size.
@@ -106,24 +118,28 @@ class Sound:
             save(0, suffix, np.pad(self.y, (0, step_size - n)))
         else:
             for i in range(n // step_size):
-                save(0, suffix, self.y[i * step_size: (i + 1) * step_size])
-            if (n % step_size != 0):  # handling of last sample
+                save(0, suffix, self.y[i * step_size : (i + 1) * step_size])
+            if n % step_size != 0:  # handling of last sample
                 i = n // step_size
                 if mode == 'drop':
                     pass
                 elif mode == 'duplicate':
-                    save(0, suffix, self.y[n - step_size: n])
+                    save(0, suffix, self.y[n - step_size : n])
                 elif mode == 'keep':
-                    save(0, suffix, self.y[i * step_size: n])
+                    save(0, suffix, self.y[i * step_size : n])
 
 
 if __name__ == '__main__':
-    folder = Path('../data/urban/processed_1D/talk')
+    folder = Path('../data/UrbanSound8K/processed/4')
     for file in folder.glob('*.wav'):
         print(file)
-        s1 = Sound(file, process=False, sampling_rate=None)
+        s1 = Sound(file, process=True, sampling_rate=None)
         # s1.plot_spectrogram()
         # plt.show()
         print(s1.y.shape)
         print(s1.sampling_rate)
+        print(s1.mfcc.shape)
+        s1.spectrogram = s1.mfcc
+        s1.plot_spectrogram()
+        plt.show()
         # s1.to_samples(sample_length=4, mode='duplicate')
